476fd612b06de34cce32fd899161bb7ebf0a2b07
Dean <72749793+drossano@users.noreply.github.com>
2023-04-25 12:31:46 -0400

    Add username field on sign up, display username as post author when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "posts#index"
   
-  devise_for :users
+  devise_for :users, :controllers => {
+    registrations: 'registrations'
+  }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create, :index]
